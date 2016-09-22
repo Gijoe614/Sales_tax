905422969464ab486a169180018ff5f84dc3361b
Gijoe614 <jvandoorn@students.mchenry.edu>
2016-09-22 14:05:24 -0500

    Add files via upload

---
+++ Sales-tax.py
@@ -0,0 +1,31 @@
+cost = 1
+totalCost = 0
+
+
+print("Input prices. Enter 0 to indicate that you are done entering.")
+
+
+while cost != 0:
+    strCost = raw_input("Enter the cost of the item: ")
+
+    cost = float(strCost)
+
+    totalCost += cost
+else:
+
+    print ("The total is $" + str(totalCost))
+
+
+strTax = raw_input("How much will you be taxed? Enter whole numbers, I will treat it as a percent: ")
+
+
+sales_tax = float(strtax) * 0.6
+
+
+total = totalCost * sales_tax
+print("You will be taxed " + strTax + "%")
+
+print("That is $" + str(round(total, 2)))
+grandTotal = float(total) + totalCost
+
+print("The total cost, with tax, is: $" + str(round(grandTotal, 2)))
